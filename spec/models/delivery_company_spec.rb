fa976919137d55805bacdd57974443ea35b4691c
Felipe Michelon <felipe.michelon@outlook.com>
2020-04-25 14:04:48 -0300

    Fix typo on spec/models/delivery_company_spec.rb
    Changed 'subjet' to 'subject'

--- spec/models/delivery_company_spec.rb
+++ spec/models/delivery_company_spec.rb
@@ -5,32 +5,32 @@ require_relative '../factories/delivery_company'
 
 RSpec.describe DeliveryCompany do
   context 'when the model is valid' do
-    let(:subjet) { build(:delivery_company) }
+    let(:subject) { build(:delivery_company) }
 
     it 'is saved' do
-      expect(subjet.valid?).to eq true
+      expect(subject.valid?).to eq true
     end
 
     it 'can be saved' do
-      expect(subjet.save).to eq true
+      expect(subject.save).to eq true
     end
   end
 
   context 'when the model is not valid' do
     context 'because it has no name' do
-      let(:subjet) { build(:delivery_company, name: '') }
+      let(:subject) { build(:delivery_company, name: '') }
 
       it 'cannot be saved' do
-        expect(subjet.valid?).to eq false
+        expect(subject.valid?).to eq false
       end
     end
 
     context 'because it has a name that is already used' do
       let(:already_existing_record) { create(:delivery_company) }
-      let(:subjet) { build(:delivery_company, name: already_existing_record.name) }
+      let(:subject) { build(:delivery_company, name: already_existing_record.name) }
 
       it 'cannot be saved' do
-        expect(subjet.valid?).to eq false
+        expect(subject.valid?).to eq false
       end
     end
   end
