aaf77565344caf8d2c83c054f93efa368394b57a
Felipe Michelon <felipe.michelon@outlook.com>
2020-06-27 20:53:29 -0300

    Create lambda to remind to update NotificationRequests

---
+++ infra/lambdas/remind_update_notification_requests/lambda_function.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+load_paths = Dir['./vendor/bundle/ruby/*/gems/**/lib']
+$LOAD_PATH.unshift(*load_paths)
+
+require 'nokogiri'
+
+def lambda_handler(event:, context:)
+  uri = URI.parse("#{ENV['BACKEND_URL']}/reminders/update_notification_requests")
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = true
+  request = Net::HTTP::Post.new(uri.request_uri)
+  http.request(request)
+end
