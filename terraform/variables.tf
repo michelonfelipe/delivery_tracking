51e5bb2e11e40dbe7543f8346f9076ae2699af66
Felipe Michelon <felipe.michelon@outlook.com>
2020-06-28 20:22:18 -0300

    Remove unused infra sns and sqs dependencies
    All sns and sqs infra, that has no use on this moment, is removed

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -55,23 +55,7 @@ variable "lambda_backend_url" {
 }
 
 # SNS
-variable "sns_topic_name_notification_request_creation" {
-  description = "The name of the sns topic notification_request_creation"
-  type        = string
-}
-
 variable "sns_topic_name_notification_request_update" {
   description = "The name of the sns topic notification_request_update"
-  type        = string
-}
-
-# SQS
-variable "sqs_queue_name_notification_request_creation" {
-  description = "The name of the sqs queue notification_request_creation"
-  type        = string
-}
-
-variable "sqs_queue_name_notification_request_creation_deadletter" {
-  description = "The name of the sqs queue notification_request_creation_deadletter"
   type        = string
 }
