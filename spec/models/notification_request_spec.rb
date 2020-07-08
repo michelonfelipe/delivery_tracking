22d36bc86a248ac5547070b0a2e8bcd5dafbb9e4
Felipe Michelon <felipe.michelon@outlook.com>
2020-07-07 21:28:37 -0300

    Fix NotificationRequest inactive scope
    The scope was not considering the status on the query

--- spec/models/notification_request_spec.rb
+++ spec/models/notification_request_spec.rb
@@ -84,7 +84,7 @@ RSpec.describe NotificationRequest do
   context 'when it is inactive' do
     before(:each) do
       create_list(
-        :notification_request, 2, status: NotificationRequest::STATUSES[:ACTIVE]
+        :notification_request, 1, status: NotificationRequest::STATUSES[:ACTIVE]
       ).each do |nr|
         create(
           :notification_request_status, notification_request: nr, created_at: DateTime.now
@@ -93,6 +93,24 @@ RSpec.describe NotificationRequest do
 
       create_list(
         :notification_request, 2, status: NotificationRequest::STATUSES[:ACTIVE]
+      ).each do |nr|
+        create(
+          :notification_request_status,
+          notification_request: nr,
+          created_at: DateTime.now - NotificationRequest::DAYS_TO_BE_CONSIDERED_INACTIVE
+        )
+      end
+
+      create_list(
+        :notification_request, 3, status: NotificationRequest::STATUSES[:DONE]
+      ).each do |nr|
+        create(
+          :notification_request_status, notification_request: nr, created_at: DateTime.now
+        )
+      end
+
+      create_list(
+        :notification_request, 4, status: NotificationRequest::STATUSES[:DONE]
       ).each do |nr|
         create(
           :notification_request_status,
